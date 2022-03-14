8a4d9d518b9c0b41d0e88a17909e3e296bb96804
akj009 <develop.abhij03@gmail.com>
2022-03-15 00:56:27 +0530

    print nums problem

---
+++ recursion/04. Practice/2_print_all_sums.js
@@ -0,0 +1,38 @@
+/**
+ * 1. Given a number n, print all the number combinations from
+ *    1 to n whose sum is equal to n. Numbers can be repeated.
+ *
+ * Example for n = 5, the output should have the following-
+ *      [ 1, 1, 1, 1, 1 ]
+ *      [ 1, 1, 1, 2 ]
+ *      [ 1, 1, 2, 1 ]
+ *      [ 1, 1, 3 ]
+ *      [ 1, 2, 1, 1 ]
+ *      [ 1, 2, 2 ]
+ *      [ 1, 3, 1 ]
+ *      [ 1, 4 ]
+ *      [ 2, 1, 1, 1 ]
+ *      [ 2, 1, 2 ]
+ *      [ 2, 2, 1 ]
+ *      [ 2, 3 ]
+ *      [ 3, 1, 1 ]
+ *      [ 3, 2 ]
+ *      [ 4, 1 ]
+ *      [ 5 ]
+ */
+
+
+
+
+
+
+/**
+ * 2. Given a number n, print all the number combinations from
+ *    1 to n whose sum is equal to n. Numbers cannot be repeated.
+ *
+ * Example, for n = 5, the output should have the following-
+ *
+ *      [ 1, 4 ]
+ *      [ 2, 3 ]
+ *      [ 5 ]
+ */
