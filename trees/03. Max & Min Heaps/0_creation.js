ecc45cb05937e8eebb8122a3c4f8ea17be19b970
akj009 <develop.abhij03@gmail.com>
2022-04-02 12:58:13 +0530

    max heap using arrays

--- trees/03. Max & Min Heaps/0_creation.js
+++ trees/03. Max & Min Heaps/0_creation.js
@@ -21,22 +21,30 @@ class MinHeapUsingArrays {
         return this.heap[1];
     }
 
+    /**
+     * time complexity of addition in min heap: log n
+     *
+     */
     add(data) {
         this.heap.push(data);
+        this.#heapify();
+    }
+
+    #heapify() {
         if (this.heap.length > 1) {
             let curr = this.heap.length - 1;
             while (curr > 1 &&
-                this.heap[Math.floor(curr/2)] > this.heap[curr]) {
+            this.heap[Math.floor(curr / 2)] > this.heap[curr]) {
 
-                [this.heap[Math.floor(curr/2)], this.heap[curr]]
-                    = [this.heap[curr], this.heap[Math.floor(curr/2)]];
-                curr = Math.floor(curr/2);
+                [this.heap[Math.floor(curr / 2)], this.heap[curr]]
+                    = [this.heap[curr], this.heap[Math.floor(curr / 2)]];
+                curr = Math.floor(curr / 2);
             }
         }
     }
 
     print() {
-        console.log(this.heap);
+        console.log('min-heap:', this.heap);
     }
 }
 
@@ -45,6 +53,49 @@ let minHeap = new MinHeapUsingArrays();
 for (let i = 0; i < inputArr.length; i++) {
     minHeap.add(inputArr[i]);
     minHeap.print();
+}
+
+class MaxHeapUsingArrays {
+    constructor() {
+        this.heap = [null];
+    }
+
+    getMax() {
+        return this.heap[1];
+    }
+
+    /**
+     * time complexity of addition in min heap: log n
+     *
+     */
+    add(data) {
+        this.heap.push(data);
+        this.#heapify();
+    }
+
+    #heapify() {
+        if (this.heap.length > 1) {
+            let curr = this.heap.length - 1;
+            while (curr > 1 &&
+            this.heap[Math.floor(curr / 2)] < this.heap[curr]) {
+
+                [this.heap[Math.floor(curr / 2)], this.heap[curr]]
+                    = [this.heap[curr], this.heap[Math.floor(curr / 2)]];
+                curr = Math.floor(curr / 2);
+            }
+        }
+    }
+
+    print() {
+        console.log('max-heap:', this.heap);
+    }
+}
+
+inputArr = [23, 32, 38, 10, 36, 45, 57];
+let maxHeap = new MaxHeapUsingArrays();
+for (let i = 0; i < inputArr.length; i++) {
+    maxHeap.add(inputArr[i]);
+    maxHeap.print();
 }
 
 
